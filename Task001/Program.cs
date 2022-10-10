e8228b4a6477632958d746db1d880a73931f5977
Anna <ankakras@yandex.ru>
2022-10-10 18:46:47 +0700

    Решение задачи 001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,1 +1,32 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели ");
+string day = Convert.ToString(Console.ReadLine());
+if(day == "1")
+{
+    Console.WriteLine("День недели рабочий");
+}
+else if(day == "2")
+{
+    Console.WriteLine("День недели рабочий");
+}
+else if(day == "3")
+{
+    Console.WriteLine("День недели рабочий");
+}
+else if(day == "4")
+{
+    Console.WriteLine("День недели рабочий");
+}
+else if(day == "5")
+{
+    Console.WriteLine("День недели рабочий");
+}
+else if(day == "6")
+{
+    Console.WriteLine("День недели выходной");
+}
+else if(day == "7")
+{
+    Console.WriteLine("День недели выходной");
+}
