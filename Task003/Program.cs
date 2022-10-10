89f28fa7601f626e0132e41bf6cb5a6fc7fb4d3c
Anna <ankakras@yandex.ru>
2022-10-10 20:01:58 +0700

    Решение задачи 003

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Задайте номер четверти- ");
+string n = Console.ReadLine();
+if(n == "1")
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if(n == "2")
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if(n == "3")
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if(n == "4")
+{
+    Console.WriteLine("x > 0, y < 0");
+}
